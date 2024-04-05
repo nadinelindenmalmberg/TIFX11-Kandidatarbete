ee5b1f920eb45c70170773d9c1f4a8af655b0a5b
mayeralex <murmis@hotmail.se>
2024-04-05 20:05:18 +0200

    tar nu info automatiskt info från filerna

--- 3d_2d_cameracali.py
+++ 3d_2d_cameracali.py
@@ -1,36 +1,60 @@
 import numpy as np
 import cv2
-import csv
 import matplotlib.pyplot as plt
 import pandas as pd
+import json
 
 
 csv_file = 'jonte-exp.csv'
+camera_filename = r'240403-P-v9_camerainfo.json'
+camera_i = 9
+
+with open(camera_filename,'r') as f:
+    camera_file = json.load(f)
+
+# print('Choose camera:')
+# camera_i = int(input('> '))
+if not camera_file['Cameras'][camera_i]["Model"] == 'Miqus Video':
+    raise Exception('The chosen camera is not a video camera!')
+
+camera_intrinsics = camera_file['Cameras'][camera_i]['Intrinsic']
 
 
 #inställningarna för kamera 10 när jag springer sista gången 
 
 # Camera matrix
 camera_matrix = np.array([
-    [71559.046875/64, 0, 60938.535156/64], 
-    [0, 71578.187500/64, 34490.714844/64], 
-    [0, 0, 1] 
-])
+    [camera_intrinsics['FocalLengthU'], 0, camera_intrinsics['CenterPointU']], 
+    [0, camera_intrinsics['FocalLengthU'], camera_intrinsics['CenterPointV']], 
+    [0, 0, 64] 
+]) / 64
+# camera_matrix = np.array([
+#     [71559.046875/64, 0, 60938.535156/64], 
+#     [0, 71578.187500/64, 34490.714844/64], 
+#     [0, 0, 1] 
+# ])
 
 # Distortion coefficients
-dist_coeffs = np.array([
-    -0.001828, -0.001020, 0.000278, 0.000274, 0.0  
-])
+dist_coeffs = np.array(
+    [camera_intrinsics[key] for key in ["RadialDistortion1","RadialDistortion2","TangentalDistortion1","TangentalDistortion2","RadialDistortion3"] ]
+)
+# dist_coeffs = np.array([
+#     -0.001828, -0.001020, 0.000278, 0.000274, 0.0  
+# ])
 
 # Translation vector
-t_vec = np.array([996.594238, -1437.977905, 1048.386230])
+t_vec = np.array([camera_file['Cameras'][camera_i]['Transform'][key] for key in ['x','y','z']])
+# t_vec = np.array([996.594238, -1437.977905, 1048.386230])
 
 # Rotation matrix
 R = np.array([
-    [0.999686, -0.014467, -0.020452],
-    [0.021407, 0.069319, 0.997365],
-    [-0.013011, -0.997490, 0.069607]
-])
+    [camera_file['Cameras'][camera_i]['Transform'][key] for key in ['r11','r12','r13','r21','r22','r23','r31','r32','r33']]
+]).reshape(3,3)
+# R = np.array([
+#     [0.999686, -0.014467, -0.020452],
+#     [0.021407, 0.069319, 0.997365],
+#     [-0.013011, -0.997490, 0.069607]
+# ])
 
 
 # kod för att ta ut en viss frames (rad) projecering
@@ -53,31 +77,35 @@ def read_from_csv(file_path, rows=None):
     return coordinates
 
 """
+row_index = 0
+df = pd.read_csv(csv_file)
+row = df.iloc[row_index].to_numpy()
 
+points_3d = row.reshape(int(len(row)/3) ,3)
 
 # Testade att manuellt ta ut punkterna för frame 1 och se om det blev bättre
 
-points_3d = np.array([
-    [869.232,514.902,1126.789],
-    [968.763,705.824,1098.568],
-    [1245.429,601.803,640.850],
-    [971.445,616.964,289.955],
-    [997.111,361.619,1085.016],
-    [834.924,460.139,558.521],
-    [561.038,428.078,218.277],
-    [891.158,509.021,1386.855],
-    [1023.155,554.592,1504.854],
-    [1062.355,555.889,1667.501],
-    [1117.334,558.549,1776.278],
-    [1110.373,385.665,1546.745],
-    [1114.639,224.417,1250.126],
-    [1208.994,414.587,1370.150],
-    [983.519,728.815,1572.846],
-    [688.716,750.973,1397.713],
-    [885.149,772.523,1244.889],
-    [657.024,466.363,73.483],
-    [1096.239,627.014,166.702]
-], dtype=np.float32).reshape(-1, 1, 3)
+# points_3d = np.array([
+#     [869.232,514.902,1126.789],
+#     [968.763,705.824,1098.568],
+#     [1245.429,601.803,640.850],
+#     [971.445,616.964,289.955],
+#     [997.111,361.619,1085.016],
+#     [834.924,460.139,558.521],
+#     [561.038,428.078,218.277],
+#     [891.158,509.021,1386.855],
+#     [1023.155,554.592,1504.854],
+#     [1062.355,555.889,1667.501],
+#     [1117.334,558.549,1776.278],
+#     [1110.373,385.665,1546.745],
+#     [1114.639,224.417,1250.126],
+#     [1208.994,414.587,1370.150],
+#     [983.519,728.815,1572.846],
+#     [688.716,750.973,1397.713],
+#     [885.149,772.523,1244.889],
+#     [657.024,466.363,73.483],
+#     [1096.239,627.014,166.702]
+# ], dtype=np.float32).reshape(-1, 1, 3)
 
 
 
